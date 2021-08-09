89a612726c20d934a9db6a2b9a736ffd5fa8fd4e
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-08-09 14:15:06 +0500

    init pg and close pg

--- app/db.py
+++ app/db.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+import aiopg
 from sqlalchemy import MetaData, Table, Integer, Column, String, Text, DateTime
 
 meta = MetaData()
@@ -15,3 +16,20 @@ advertisement = Table(
     Column('creator_id', Integer, db.ForeignKey('user.id')),
     Column('created_on', DateTime, default=datetime.utcnow)
 )
+
+
+async def init_pg(app):
+    conf = app['config']['postgres']
+    engine = await aiopg.sa.create_engine(
+        database=conf['database'],
+        user=conf['user'],
+        password=conf['password'],
+        host=conf['host'],
+        port=conf['port'],
+    )
+    app['db'] = engine
+
+
+async def close_pg(app):
+    app['db'].close()
+    await app['db'].wait_closed()
--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-from aiohttp import web
+from aiohttp import web, WSCloseCode
+
+from app.db import init_pg, close_pg
 from app.routes import setup_routes
+import weakref
+
+from app.settings import config
+
 
 app = web.Application()
 setup_routes(app)
-web.run_app(app)
+app['config'] = config
+
+app.on_startup.append(init_pg)
+app.on_cleanup.append(close_pg)
+
+web.run_app(app, host='127.0.0.1', port=8081)
